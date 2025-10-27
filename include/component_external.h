8fa082629e3dc67dffd557eaafc27b921059a7a1
Graeme Kennedy <gkennedy9@gatech.edu>
2025-10-27 15:00:29 -0400

    updated the template for the external component

--- include/component_external.h
+++ include/component_external.h
@@ -18,7 +18,12 @@ class ComponentExtneral : public ComponentGroupBase<T> {
         constraints(constraints),
         jacobian(jacobian) {}
 
-  virtual T lagrangian(const Vector<T>& data, const Vector<T>& x) const {
+  // Update the design point - applying the callbacks
+  // void update(const Vector<T>& x) { update_callback(x, constraints,
+  // jacobian); }
+
+  // Compute the Lagrangian
+  T lagrangian(const Vector<T>& data, const Vector<T>& x) const {
     T value(0.0);
     for (int i = 0; i < size; i++) {
       T lambda = x[con_indices[i]];
@@ -26,16 +31,53 @@ class ComponentExtneral : public ComponentGroupBase<T> {
     }
     return value;
   }
-  virtual void add_gradient(const Vector<T>& data, const Vector<T>& x,
-                            Vector<T>& g) const {
-    // Compute the Hessian-vector product
-    for (int i = 0; i < ncon; i++) {
+  void add_gradient(const Vector<T>& data, const Vector<T>& x,
+                    Vector<T>& g) const {
+    for (int i = 0; i < nrows; i++) {
+      int row = con_indices[i];
+      g[row] += constraints[i];
+
+      for (int jp = rowp[i]; jp < rowp[i + 1]; jp++) {
+        int j = cols[jp];
+        int col = var_indices[j];
+
+        T lambda = x[row];
+        g[col] += A[jp] * lambda;
+      }
     }
   }
-  virtual void add_hessian_product(const Vector<T>& data, const Vector<T>& x,
-                                   const Vector<T>& p, Vector<T>& h) const {}
-  virtual void add_hessian(const Vector<T>& data, const Vector<T>& x,
-                           const NodeOwners& owners, CSRMat<T>& mat) const {}
+  void add_hessian_product(const Vector<T>& data, const Vector<T>& x,
+                           const Vector<T>& p, Vector<T>& h) const {
+    for (int i = 0; i < nrows; i++) {
+      int row = con_indices[i];
+
+      for (int jp = rowp[i]; jp < rowp[i + 1]; jp++) {
+        int j = cols[jp];
+        int col = var_indices[j];
+
+        T px = p[col];
+        T plambda = p[row];
+        h[col] += A[jp] * plambda;
+        h[row] += A[jp] * px;
+      }
+    }
+  }
+  void add_hessian(const Vector<T>& data, const Vector<T>& x,
+                   const NodeOwners& owners, CSRMat<T>& mat) const {
+    // Add the contributions to the Hessian matrix...
+  }
+
+  void get_constraint_csr_data(int* nrows, int* ncols, const int* rows[],
+                               const int* columns[], const int* rowp[],
+                               const int* cols[]) const {
+    if (rows) {
+      *rows = con_indices->get_array();
+    }
+    if (columns) {
+      *columns = var_indices->get_array();
+    }
+    jacobian->get_data(nrows, ncols, rowp, cols);
+  }
 
   std::shared_ptr<Vector<T>> get_constraints() { return constraints; }
   std::shared_ptr<CSRMat<T>> get_jacobian() { return jacobian; }
