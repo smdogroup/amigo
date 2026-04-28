57b6daff637310c6c4b96c7265bdb1d3660abaa1
gkennedy <graeme.kennedy@ae.gatech.edu>
2026-04-27 20:39:24 -0400

    updated test order

--- tests/unit/test_ldl.py
+++ tests/unit/test_ldl.py
@@ -131,7 +131,8 @@ def test_sparse_ldl():
             ustab=0.4,
             order=am.OrderingType.NATURAL,
         )
-        ldl.factor()
+        flag = ldl.factor()
+        assert flag == 0
 
         ldl_inertia = ldl.get_inertia()
         ldl.solve(xvec)
@@ -157,7 +158,8 @@ def test_sparse_ldl_amd():
             ustab=0.4,
             order=am.OrderingType.AMD,
         )
-        ldl.factor()
+        flag = ldl.factor()
+        assert flag == 0
 
         ldl_inertia = ldl.get_inertia()
         ldl.solve(xvec)
@@ -183,7 +185,8 @@ def test_sparse_poisson():
             ustab=0.4,
             order=am.OrderingType.NATURAL,
         )
-        ldl.factor()
+        flag = ldl.factor()
+        assert flag == 0
 
         ldl_inertia = ldl.get_inertia()
         ldl.solve(xvec)
@@ -210,7 +213,8 @@ def test_sparse_poisson_amd():
             delay_growth=1.0,
             order=am.OrderingType.AMD,
         )
-        ldl.factor()
+        flag = ldl.factor()
+        assert flag == 0
 
         ldl_inertia = ldl.get_inertia()
         ldl.solve(xvec)
@@ -221,36 +225,6 @@ def test_sparse_poisson_amd():
         assert ldl_inertia[0] == inertia[0]
         assert ldl_inertia[1] == inertia[1]
         assert err < 1e-15
-
-
-def test_sparse_ldl_poisson_rhs():
-    stype = am.SolverType.LDL
-    for n in [10, 20, 50]:
-        np.random.seed(0)
-        csr, xsoln, rhs, inertia = get_poisson_2d(n)
-
-        nrhs = 7
-        X = np.zeros((len(xsoln), nrhs), order="F")
-        for k in range(nrhs):
-            X[:, k] = rhs
-
-        ldl = am.SparseLDL(
-            csr, solver_type=stype, ustab=0.4, order=am.OrderingType.AMD
-        )
-        ldl.factor()
-
-        if stype == am.SolverType.LDL:
-            ldl_inertia = ldl.get_inertia()
-            assert ldl_inertia[0] == inertia[0]
-            assert ldl_inertia[1] == inertia[1]
-
-        ldl.solve(X)
-
-        err = 0.0
-        for k in range(nrhs):
-            err = max(err, np.linalg.norm(X[:, k] - xsoln))
-
-        assert err < 1e-11
 
 
 def test_sparse_cholesky_poisson_rhs():
@@ -264,10 +238,35 @@ def test_sparse_cholesky_poisson_rhs():
         for k in range(nrhs):
             X[:, k] = rhs
 
-        ldl = am.SparseLDL(
-            csr, solver_type=stype, ustab=0.4, order=am.OrderingType.AMD
-        )
-        ldl.factor()
+        ldl = am.SparseLDL(csr, solver_type=stype, ustab=0.4, order=am.OrderingType.AMD)
+        flag = ldl.factor()
+
+        assert flag == 0
+
+        ldl.solve(X)
+
+        err = 0.0
+        for k in range(nrhs):
+            err = max(err, np.linalg.norm(X[:, k] - xsoln))
+
+        assert err < 1e-11
+
+
+def test_sparse_ldl_poisson_rhs():
+    stype = am.SolverType.LDL
+    for n in [10, 20, 50]:
+        np.random.seed(0)
+        csr, xsoln, rhs, inertia = get_poisson_2d(n)
+
+        nrhs = 7
+        X = np.zeros((len(xsoln), nrhs), order="F")
+        for k in range(nrhs):
+            X[:, k] = rhs
+
+        ldl = am.SparseLDL(csr, solver_type=stype, ustab=0.4, order=am.OrderingType.AMD)
+        flag = ldl.factor()
+
+        assert flag == 0
 
         if stype == am.SolverType.LDL:
             ldl_inertia = ldl.get_inertia()
